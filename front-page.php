e223dcef094b03ce940c24570a1f21b73c9a397d
Anne-Deborah Binggeli <ad.binggeli@gamil.com>
2024-05-24 19:49:58 +0200

    Ajout de la section Nomination aux Oscars.

--- front-page.php
+++ front-page.php
@@ -65,6 +65,12 @@ get_header();
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
             </section>
+            
+            <!--Section Nomination aux Oscars-->
+            <?php
+            get_template_part('temple-parts/nomination'); //Appel du fichier nomination.php
+            ?>
+            
     </main><!-- #main -->
 
 <?php
